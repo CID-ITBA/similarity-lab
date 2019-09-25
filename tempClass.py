b5dd963531c6abd607a2016cd8a0d6b211bcb9df
cselmo <cselmo@itba.edu.ar>
2019-09-25 08:08:14 -0300

    models added

--- tempClass.py
+++ tempClass.py
@@ -12,6 +12,11 @@
 
 import numpy as np
 from scipy import spatial
+from os import listdir
+from os.path import isfile, join
+import pickle
+import operator
+import collections
 
 '''
 def validate(matrixes, vocabulary, yearDict):
@@ -24,23 +29,33 @@ def validate(matrixes, vocabulary, yearDict):
 '''
     
 class tempName:
-    def __init__(self, matrixes, yearDict, vocabulary):
-        self.vocabulary = vocabulary # vocabulario utilizado para generar las matrices
-        self.inverseVocab = dict(map(reversed, vocabulary.items())) # diccionario inverso del vocabulario
-        print(self.inverseVocab)
-        self.matrixes = matrixes    # matrices de probabilidad de coocurrencia por year
-        self.yearDict = yearDict    # diccionario que vincula year con indice en matrixes
+    def __init__(self, model="dw2v", dataset="nyt"):
+        with open("models/{}/{}/dictionary.pck".format(model,dataset),"rb") as f:
+            dictionary = pickle.load(f)
+        self.vocabulary = dictionary # vocabulario utilizado para generar las matrices
+        self.inverseVocab = dict(map(reversed, dictionary.items())) # diccionario inverso del vocabulario
+        mypath = "models/{}/{}/".format(model,dataset)
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        self.cant_slices = len(onlyfiles)-1
+        self.yearDict = {file.split("-")[1][:-4]:int(file.split("-")[0]) for file in onlyfiles if file != "dictionary.pck"}
+        self.matrices = list(range(self.cant_slices))
+        for file in onlyfiles:
+            if file != "dictionary.pck":
+                idx=int(file.split("-")[0])
+                self.matrices[idx] = np.load("models/{}/{}/".format(model,dataset)+file)    # matrices de probabilidad de coocurrencia por year
+        self.model = model
+        self.dataset = dataset
 
     def findSimilars(self, vector, threshold, year):
         if threshold > 0:
-            tempMat = self.matrixes[self.yearDict[year]] # obtengo la matriz del anio pedido
+            tempMat = self.matrices[self.yearDict[year]] # obtengo la matriz del anio pedido
             results = {} # container para los resultados encontrados dict{string, sim}
             # np.transpose(tempMat) si la palabra es la columna
             if type(threshold) is float:
                 if threshold < 1.0:
                     for index, word in enumerate(tempMat): # ASUMIENDO QUE LA PALABRA ES LA FILA!!!
                         cosSim = 1 - spatial.distance.cosine(vector, word) # se obtiene la similitud coseno entre word y vector
-                        if np.abs(cosSim) > threshold: # si la similitud coseno cae dentro del threshold dado
+                        if cosSim > threshold: # si la similitud coseno cae dentro del threshold dado
                             results[ self.inverseVocab[index] ] = cosSim # se guarda en results la palabra encontrada
                 else:
                     raise ValueError("Similarity Threshold value must be under 1.0")
@@ -53,6 +68,7 @@ class tempName:
                 mostSimilar = similarities[0:threshold] # se guardan en mostSimilar los 'threshold' elementos con mayor similitud
                 for index, cosSim in mostSimilar:
                     results[ self.inverseVocab[index] ] = cosSim
+            results = sorted(results.items(), key = operator.itemgetter(1), reverse = True) #orders the results
             return results
 
         else:
@@ -64,13 +80,15 @@ class tempName:
         return histograma
 
     def getVector(self, word, year):
-        vector = []
-
+        vector = self.matrices[self.yearDict[year]][self.vocabulary[word]]
         return vector
 
-    def getVectorPosNeg(self, positives, negatives, year):
-        vector = []
-
+    def getVectorPosNeg(self, year, positives=[], negatives=[]):
+        vector = np.zeros(self.matrices[self.yearDict[year]][0].shape)
+        for word in positives:
+            vector += self.matrices[self.yearDict[year]][self.vocabulary[word]]
+        for word in negatives:
+            vector -= self.matrices[self.yearDict[year]][self.vocabulary[word]]
         return vector
 
     def getSim(self, w1, y1, w2, y2):
@@ -82,17 +100,3 @@ class tempName:
     def getEvolByStep(self, word):
         evolution = []
         return evolution
-
-ma = [[-1,-2,-3],[4,5,6],[7,8,9]]
-mb = [[1,2.1,3],[4.2,4.8,6],[7.02,8,9.3]]
-mc = [[1.1,2.2,3.1],[4.23,5,6],[7.03,8,9.32]]
-
-matrixes = [ma, mb, mc]
-yearDict = {1990:0, 1991:1, 1995:2}
-vocabulary = {'martin':0, 'pablo':1, 'carlos':2}
-
-tempObject = tempName(matrixes, yearDict, vocabulary)
-
-newVec = tempObject.findSimilars([1,2,3], 3, 1990)
-
-print(newVec)
