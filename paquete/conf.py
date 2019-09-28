ef57247f11313af98740f1fd85988b0c08b42caf
MT2321 <mrodriguez@itba.edu.ar>
2019-09-28 00:19:23 -0300

    Update config y cambio de Theme

--- paquete/conf.py
+++ paquete/conf.py
@@ -12,14 +12,14 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('C:/Users/RA/Documents/GitHub/cid-ProjectZero/paquete'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'CLASEHTML'
-copyright = '2019, p y m'
-author = 'p y m'
+project = 'SimilarityLab'
+copyright = '2019, Me'
+author = 'Me'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,13 +28,14 @@ author = 'p y m'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -42,7 +43,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
